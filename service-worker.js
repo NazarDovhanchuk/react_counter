eafaf51507cf99144c5be514a93e17f66fa9ad2c
Nazar <nazardovhanchuk@gmail.com>
2021-01-20 21:20:13 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.d9c3fa3d18ff7135a356a9cc2a320f44.js"
 );
 
 self.addEventListener('message', (event) => {
